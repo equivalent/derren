fd17c3c483223d2ae7eea1487e6c241191e6462a
Tomas Valent <equivalent@eq8.eu>
2018-12-22 23:03:58 +0100

    stash progress till AZ cli issue is resolved https://github.com/Azure/azure-cli/issues/8119

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 # Derren
 
 Deploy static websites on Azure Blob.  ...it's freaking magic !
+
+
+
+```
+az extension add --name storage-preview
+```
--- lib/derren.rb
+++ lib/derren.rb
@@ -1,21 +1,40 @@
 module Derren
   def self.call
-    conf = Config.new('./config.yml')
-    p conf.app.name
-    p conf.app.spas.first.app_path
+    config = Config.new('./config.yml')
+    puts "Worknig with application: #{config.app.name}"
+
+    binding.irb
+    unless config.app.exist?
+      config.app.create
+    end
+
+    config.app.spas.first(1).each do |spa|
+      spa.setup
+    end
   end
 end
 
 
 module Derren
   class SPA
-    def initialize(name)
+    def initialize(name, config:)
       @name = name
+      @config = config
     end
 
-    attr_reader :name
+    attr_reader :name, :config
     attr_accessor :project_folder_path
     attr_writer :app_path
+
+    def setup
+      cmd = "az storage blob service-properties update --account-name #{config.app.name} --static-website --404-document index.html --index-document index.html"
+      binding.irb
+      res1=  system(cmd)
+
+      res2 = `az storage account show -n #{config.app.name} -g #{config.resource_group} --query "primaryEndpoints.web"`
+      binding.irb
+        
+    end
 
     def app_path
       @app_path
@@ -25,10 +44,25 @@ end
 
 module Derren
   class Application
-    attr_reader :name
+    attr_reader :name, :config
 
-    def initialize(name)
+    def initialize(name, config: config)
       @name = name
+      @config = config
+    end
+
+
+    def create
+      if system("az storage account create --name #{name} -g #{config.resource_group}")
+        true
+      else
+        raise "Was not able to create storage account #{name}"
+      end
+    end
+
+    def exist?
+      res = `az storage account check-name --name #{name}`
+      JSON.parse(res.to_s)['nameAvailable'] # true/false
     end
 
     def spas
@@ -45,14 +79,18 @@ module Derren
       @path = path_to_config
     end
 
+    def resource_group
+      conf.fetch('resource_group')
+    end
+
     def app
       app_hash = conf.fetch('app')
-      app = Application.new(app_hash.fetch('name'))
+      app = Application.new(app_hash.fetch('name'), config: self)
 
       app_hash
         .fetch('spas')
         .each do |name, spa_hash|
-          spa = SPA.new(name)
+          spa = SPA.new(name, config: self)
           spa.app_path = spa_hash['app_path']
           spa.project_folder_path = spa_hash.fetch('project_folder_path')
 
