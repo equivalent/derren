ea9849b9d1173a307cae96c6072a2d1058991600
Tomas Valent <equivalent@eq8.eu>
2018-12-25 18:07:00 +0100

    options

--- lib/derren.rb
+++ lib/derren.rb
@@ -1,17 +1,37 @@
 
 module Derren
-  def self.call
-    config = Config.new('./config.yml')
-    puts "Worknig with application: #{config.app.name}"
-
-    unless config.app.exist?
-      config.app.create
+  module ClassMethods
+    def call
+      config = Config.new('./config.yml')
+      puts "Worknig with application: #{config.app.name}"
     end
 
-    config.app.spas.first(1).each do |spa|
-      spa.setup
+    def main_menu
+      main_menu_setup = 'Setup'
+      main_menu_deploy = 'Deploy'
+
+      case Inputs.pick([
+        main_menu_setup,
+        main_menu_deploy])
+      when main_menu_setup
+        config.app.create unless config.app.exist?
+        config.app.spas.each { |spa| spa.setup }
+      when main_menu_deploy
+        spa = pick_spa
+        spa.deploy
+      end
+    end
+
+    private
+
+    def pick_spa
+      deploy_option_list = config.app.spas.map(&:name)
+      deploy_option = Inputs.pick(deploy_option_list)
+      spa = config.app.spas.select { |spa| spa.name == deploy_option }
     end
   end
+
+  extend ClassMethods
 end
 
 
@@ -27,30 +47,44 @@ module Derren
     attr_writer :app_path
 
     def combined_name
-      "#{config.app.name}#{name}"
+      "#{app_name}#{name}"
+    end
+
+    def app_name
+      config.app.name
+    end
+
+    def deploy
+      cmd = "az storage blob upload-batch -s #{project_folder_path} -d $web --account-name #{combined_name}"
+      if system(cmd)
+        puts "Deploy of SPA #{name} finished.\n\n#{endpoint}"
+      else
+        raise "Deploy of SPA #{name} failed"
+      end
+    end
+
+    def endpoint
+      @endpoint ||= endpoint!
+    end
+
+    def endpoint!
+      cmd = %{az storage account show --name #{combined_name} --resource-group #{app_name}}
+      show_response = `#{cmd}`
+      endpoint = JSON.parse(show_response).fetch("primaryEndpoints.web")
     end
 
     def setup
-      endpoint = nil
-      cmd = "az storage account create --name #{combined_name} --kind StorageV2 --resource-group #{config.app.name}"
+      cmd = "az storage account create --name #{combined_name} --kind StorageV2 --resource-group #{app_name}"
       if system(cmd)
         cmd = "az storage blob service-properties update --account-name #{combined_name} --static-website --404-document index.html --index-document index.html"
         if system(cmd)
-
-          cmd = %{az storage account show --name #{combined_name} --resource-group #{config.app.name}}
-          show_response = `cmd`
-          endpoint = JSON.parse(show_response).fetch("primaryEndpoints.web")
+          puts "SPA #{name} setup finished (Created under azure storage name#{combined_name}"
         else
           raise "Was not able to make #{combined_name} static website host"
         end
       else
         raise "Was not able to create storage account #{combined_name}"
       end
-      endpoint
-    end
-
-    def sync
-      # 'az storage blob upload-batch -s <SOURCE_PATH> -d $web --account-name <ACCOUNT_NAME>'
     end
 
     def app_path
